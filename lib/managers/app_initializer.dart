b368143e176f4c79b13c84d2fe56e5738ca38a59
Eyal Yaakobi <unix14@gmail.com>
2025-01-08 12:59:19 +0200

    refactored AppInitializer

--- lib/managers/app_initializer.dart
+++ lib/managers/app_initializer.dart
@@ -3,30 +3,41 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_example/common/encrypted_shared_preferences_helper.dart';
 import 'package:flutter_example/common/encryption_helper.dart';
 import 'package:flutter_example/firebase_options.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:flutter_example/common/globals.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_example/auth/authenticator.dart';
 
+import '../common/consts.dart';
+
 /// AppInitializer class is responsible for initializing the app.
 /// It ensures that the app is initialized with all the necessary services before it starts.
-/// It initializes Firebase, Google Mobile Ads, and other services can be added in the future.
+/// It initializes Firebase, Google Mobile Ads, Encryption Helper and other services can be added in the future.
 class AppInitializer {
 
   //todo add crash detection and analytics with Firebase
-  static Future<void> initialize({required Function andThen}) async {
+  static Future<void> initialize({required Function(bool) andThen}) async {
     WidgetsFlutterBinding.ensureInitialized();
+    bool isAlreadyEnteredTodos = false;
     await EncryptedSharedPreferencesHelper.initialize();
     await EncryptionHelper.initialize();
     // MobileAds.instance.initialize();
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-    //todo add auto login if user exist or anon user if not
-    await initAuthentication();
-    await initIsLoggedIn();
-    andThen();
+    try {
+      await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+      await initAuthentication();
+      await initIsLoggedIn();
+
+      var listStr = await EncryptedSharedPreferencesHelper.getString(kAllListSavedPrefs) ?? "";
+      print("initialize: load list :" + listStr);
+      isAlreadyEnteredTodos = listStr.isNotEmpty; // this is a check to see if the user has already entered the app and saved his data-> we want to let him use the app again in offline mode
+      // todo rethink offline mode maybe - we need to pop up a snackbar that defined this is an offline mode
+    } catch (exception) {
+      // firebase auth failed
+      print("Error logging in to firebase -> stay in offline mode, exception is $exception");
+      // todo show error msg
+    }
+    andThen(isAlreadyEnteredTodos);
   }
-
-
 
   static initAuthentication() async {
     /// If user exists -> Save the already logged-in user
@@ -45,14 +56,12 @@ class AppInitializer {
     handleLoginSuccess(loggedInUser!);
   }
 
-
   static initIsLoggedIn() {
     Authenticator.onAuthStateChanged.listen((event) {
       print("isLoggedInStream: $event");
       isLoggedIn = event != null && event.isAnonymous == false && event.email?.isNotEmpty == true;
     });
   }
-
 
   static handleLoginSuccess(User user) {
     currentUser = user;
